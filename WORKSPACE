fced1d043ebfa03641b93e19aa56b96722ff1575
Daniel Connelly <dhconnelly@gmail.com>
2022-10-01 13:33:07 -0400

    basic end to end scaffolding

--- WORKSPACE
+++ WORKSPACE
@@ -1,7 +1,20 @@
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 
 http_archive(
-  name = "com_google_absl",
-  urls = ["https://github.com/abseil/abseil-cpp/archive/98eb410c93ad059f9bba1bf43f5bb916fc92a5ea.zip"],
-  strip_prefix = "abseil-cpp-98eb410c93ad059f9bba1bf43f5bb916fc92a5ea",
+    name = "com_google_absl",
+    strip_prefix = "abseil-cpp-20220623.1",
+    urls = ["https://github.com/abseil/abseil-cpp/archive/refs/tags/20220623.1.zip"],
 )
+
+http_archive(
+    name = "bazel_skylib",
+    sha256 = "74d544d96f4a5bb630d465ca8bbcfe231e3594e5aae57e1edbf17a6eb3ca2506",
+    urls = [
+        "https://mirror.bazel.build/github.com/bazelbuild/bazel-skylib/releases/download/1.3.0/bazel-skylib-1.3.0.tar.gz",
+        "https://github.com/bazelbuild/bazel-skylib/releases/download/1.3.0/bazel-skylib-1.3.0.tar.gz",
+    ],
+)
+
+load("@bazel_skylib//:workspace.bzl", "bazel_skylib_workspace")
+
+bazel_skylib_workspace()
--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,59 @@
 #include <iostream>
-#include <string>
 #include <vector>
 
-#include "absl/strings/str_join.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
 
-int main() {
-  std::vector<std::string> v = {"foo", "bar", "baz"};
-  std::string s = absl::StrJoin(v, "-");
+using Buf = std::vector<char>;
+using Expr = int;
 
-  std::cout << "Joined string: " << s << "\n";
+void die(std::string_view reason) {
+    std::cerr << reason << std::endl;
+    exit(EXIT_FAILURE);
+}
 
-  return 0;
+class VM {
+   public:
+    absl::Status execute(const Buf& buf) { return absl::OkStatus(); }
+};
+
+absl::StatusOr<std::string> read_file(std::string_view path) { return ""; }
+
+absl::StatusOr<std::vector<Expr>> parse(std::string_view text) {
+    return std::vector<Expr>{};
+}
+
+absl::StatusOr<std::vector<char>> compile(const std::vector<Expr>& exprs) {
+    return std::vector<char>{};
+}
+
+void run(std::string_view path) {
+    auto text = read_file(path);
+    auto exprs = parse(text.value());
+    auto code = compile(exprs.value());
+    VM vm;
+    if (auto status = vm.execute(code.value()); !status.ok()) {
+        die(status.message());
+    }
+}
+
+void repl() {
+    VM vm;
+    std::string line;
+    while (true) {
+        std::cout << "> ";
+        if (!std::getline(std::cin, line)) break;
+        auto exprs = parse(line);
+        auto code = compile(exprs.value());
+        if (auto status = vm.execute(code.value()); !status.ok()) {
+            die(status.message());
+        }
+    }
+    std::cout << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) repl();
+    else if (argc == 2) run(argv[1]);
+    else die("usage: june <file>");
 }
